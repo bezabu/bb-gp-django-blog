068edc549c6e90214ab1e73dc9853cd2a58c0fdf
Ben Butler <benbutleranimator@gmail.com>
2024-02-26 19:07:54 +0000

    migrations

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'jBsRK5EDM6Bmy7MWHGl_-ENogCakEgCB'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-bezabu-bbgpdjangoblog-ng1m42glo68.ws-eu108.gitpod.io', '.herokuapp.com']
 
@@ -88,6 +88,13 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://*.gitpod.io"
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
